317b3ac6d86d26e7ccd3a8ca0a5a8092357e7ac9
asunguyen <cntt510@gmail.com>
2024-03-25 21:54:28 +0700

    update

--- api/controllers/gmailController.js
+++ api/controllers/gmailController.js
@@ -1,6 +1,7 @@
 const GmailModel = require("../models/gmailModel");
 const ThueMailModel = require("../models/thuemailModel");
 const authMiddl = require("../middlewares/authmidd");
+const User = require("../models/user");
 const postController = {
     getEmail: async (req, res) => {
         authMiddl.verifyToken(req, res, async() => {
@@ -41,7 +42,18 @@ const postController = {
         }
     },
     deleteEmail: async(req, res) => {
-        
+        try {
+            const mail = req.body.mail;
+            const datamail = await GmailModel.find({mail: mail});
+            if (datamail && datamail.length > 0) {
+                await GmailModel.findByIdAndDelete(datamail[0]._id);
+                res.json({code: 200, data: "Xóa thành công"});
+            } else {
+                res.json({code: 404, data: "không tìm thấy email"})
+            }
+        }catch(err) {
+            res.json({code: 500, error: "xóa mail lỗi vui lòng thử lại"});
+        }
     }
 }
 module.exports = postController;
